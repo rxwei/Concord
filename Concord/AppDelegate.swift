2dfa36a907a51453ac6e77a9ce886dbc21eb71c8
Richard Wei <xinranmsn@gmail.com>
2016-10-11 23:15:58 -0500

    Initial Commit

---
+++ Concord/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Concord
+//
+//  Created by Richard Wei on 10/11/16.
+//  Copyright © 2016 Richard Wei. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Concord/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Concord
+//
+//  Created by Richard Wei on 10/11/16.
+//  Copyright © 2016 Richard Wei. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
